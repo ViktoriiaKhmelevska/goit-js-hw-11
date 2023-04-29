f8f4f99200c891637bb92fcfb03cc71fc1b2d1e8
Viktoria Khmelevska <vikahvl@ukr.net>
2023-04-30 00:28:01 +0300

    test2

---
+++ src/api.js
@@ -0,0 +1,9 @@
+// import axios from 'axios';
+// import { inputValue, page } from './index';
+
+// const per_page = 40;
+// let totalPages = 0;
+
+
+
+// export { getImages, totalPages }
--- src/index.js
+++ src/index.js
@@ -1,87 +1,94 @@
-import { searchImages } from 'pixabay-api';
+// import { getImages, totalPages } from './api';
+import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 let inputValue = "";
-
-const API_KEY = '35804056-bdddfbb061fa747ba287fdf15';
-// const URL = "https://pixabay.com/api/?key=" + API_KEY + "&q=cats&image_type=photo&orientation=horizontal&safesearch=true";
-const URL = 'https://pixabay.com/api/?key=35804056-bdddfbb061fa747ba287fdf15&q=cats&image_type=photo&orientation=horizontal&safesearch=true';
+let page = 1;
+const per_page = 40;
+let totalPages = 0;
 
 const form = document.querySelector(".search-form");
 // const formInput = document.querySelector(".search-form input");
 const gallery = document.querySelector('.gallery');
+const lightbox = new SimpleLightbox('.gallery a');
 
 form.addEventListener('submit', onSubmit);
 
 function onSubmit(e) {
-    e.preventDefault();
-    const { searchQuery } = e.target.elements;
-
-    inputValue = searchQuery.value;
+  e.preventDefault();
+  const { searchQuery } = e.target.elements;
+  inputValue = searchQuery.value.trim();
   
-    if (!inputValue) {
-        gallery.innerHTML = "";
-        return
-    }
-
-    searchImages(URL)
-        .then((name) => {
-            console.log(name);
-        })
-        .catch((err) => {
-            Notify.failure("Sorry, there are no images matching your search query. Please try again.");
-        });
+  if (!inputValue) {
+    Notify.failure('Please, enter a search query');
+    gallery.innerHTML = "";
+    page = 1
+    return
+  }
+  addGallery();
+  return inputValue;
+};
+  
+async function addGallery() {
+  //  gallery.innerHTML = "";
+  try {
+  const response = await getImages(inputValue, page);
+    const images = response.data.hits;
+    console.log(images);
+    createMarkup(images);
+    lightbox.refresh();
+  } catch (error) {
+    console.log(error);
+ }
 };
 
-function renderGallery(arr) {
-    return arr.map(
-        ({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>    
-            ` <div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes ${likes}</b>
-    </p>
-    <p class="info-item">
-      <b>Views ${views}</b>
-    </p>
-    <p class="info-item">
-      <b>Comments ${comments}</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads ${downloads}</b>
-    </p>
-  </div>
-</div>`).join("");
- };
+// addGallery();
+  
+function createMarkup(images) {
+  const markup = images
+    .map(image => {
+      const {
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      } = image;
+      return `<a class="gallery__item" href="${largeImageURL}">
+              <div class="photo-card">
+                  <img src="${webformatURL}" alt="${tags}" loading="lazy"/>
+              </div>
+              <div class="info">
+                  <p class="info-item"><b>Likes</b> <br>${likes}</p>
+                  <p class="info-item"><b>Views</b> <br>${views}</p>
+                  <p class="info-item"><b>Comments</b> <br>${comments}</p>
+                  <p class="info-item"><b>Downloads</b> <br>${downloads}</p>
+              </div></a>`;
+    })
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', markup);
+};
 
-// searchImages("inputValue")
-
-// https://pixabay.com/api/?key=35804056-bdddfbb061fa747ba287fdf15&q=cats&image_type=photo&orientation=horizontal&safesearch=true
-// searchImages(AUTH_KEY, 'puppy').then((r) => console.log(r));
-// { totalHits: 500,
-//   hits:
-//    [ { previewHeight: 99,
-//        likes: 108,
-//        favorites: 87,
-//        tags: 'chihuahua, dog, puppy',
-//        webformatHeight: 426,
-//        views: 38770,
-//        webformatWidth: 640,
-//        previewWidth: 150,
-//        comments: 17,
-//        downloads: 11050,
-//        pageURL: 'https://pixabay.com/en/chihuahua-dog-puppy-cute-pet-621112/',
-//        previewURL: 'https://cdn.pixabay.com/photo/2015/02/02/17/06/chihuahua-621112_150.jpg',
-//        webformatURL: 'https://pixabay.com/get/ef37b00e29f61c2ad65a5854e34b4294e277eac818b5184993f0c07fafe9_640.jpg',
-//        imageWidth: 5184,
-//        user_id: 740400,
-//        user: 'Teerasuwat',
-//        type: 'photo',
-//        id: 621112,
-//        userImageURL: 'https://cdn.pixabay.com/user/2015/02/02/17-19-43-530_250x250.jpg',
-//        imageHeight: 3456 },
-//        ...
-//    ],
-//   total: 3343
-// }
+async function getImages(inputValue, page) { 
+  const API_KEY = '35804056-bdddfbb061fa747ba287fdf15';
+  const params = new URLSearchParams({
+    key: API_KEY,
+    q: inputValue,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: per_page,
+    page: page,
+  });
+    try {
+      const response = await axios.get(`https://pixabay.com/api/?${params}`);
+      totalPages = response.data.totalHits / per_page;
+    return response;
+  } catch (error) {
+    Notify.warning("Error loading.");
+  }
+};
